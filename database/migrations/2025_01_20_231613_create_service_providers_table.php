986efdca81c02215086398b14624a958ccf7d5fe
caubinho <caubinho@gmail.com>
2025-01-21 00:28:48 +0100

    Service providers, product supplier

---
+++ database/migrations/2025_01_20_231613_create_service_providers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_providers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->nullable();
+            $table->string('service_type')->nullable(); // Ex.: Consultoria, Suporte Técnico
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip')->nullable();
+            $table->boolean('client')->default(false);
+            $table->string('code_client')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_providers');
+    }
+};
---
+++ database/migrations/2025_01_20_231631_create_product_suppliers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_suppliers',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->nullable()->nullable();
+                $table->string('phone')->nullable();
+                $table->string('service_type')->nullable(); // Ex.: Consultoria, Suporte Técnico
+                $table->string('address')->nullable();
+                $table->string('city')->nullable();
+                $table->string('state')->nullable();
+                $table->string('zip')->nullable();
+                $table->string('document')->nullable();
+                $table->string('bank_account')->nullable();
+                $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_suppliers');
+    }
+};
